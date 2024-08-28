40d3a21ac52e1acfd13d130a12bb0af9760b81f0
Pedro <dsdias.pedro@gmail.com>
2024-08-28 09:09:33 -0300

    Reiniciar automaticamente

--- 1_home.py
+++ 1_home.py
@@ -2,15 +2,23 @@ import streamlit as st
 import pandas as pd
 import requests
 import json
-st. set_page_config(layout="wide")
+from io import StringIO
+import time
 
+st.set_page_config(layout="wide", page_icon="🧼", page_title="Dashboard")
+
+TEMPO = 10  # 10s eh pouco tempo, o que gasta bastante memoria do pc. Em fase final, mudar para cada 1min
 
 link = "https://dashboard-senai-default-rtdb.firebaseio.com/"
 requisicao = requests.get(f'{link}Produtos/.json')
 
-df_data = pd.read_json(requisicao.text)
+df_data = pd.read_json(StringIO(requisicao.text))
 df_data = df_data.T
 
 st.write("# VAMO LA GURIZADA, GANHADORES DO INOVA SENAI!")
 
 df_data
+
+time.sleep(TEMPO)
+
+st.rerun()
