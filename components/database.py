1cc9022846fb2c1e9d6019ae06bc8b83b5e91f5b
Pedro <dsdias.pedro@gmail.com>
2024-10-22 14:45:04 -0300

    Troca de banco de dados e utilizacao do try catch

--- components/database.py
+++ components/database.py
@@ -2,14 +2,21 @@ import mysql.connector
 
 #Fazer a conexão com o banco de dados
 def get_database():
+    # mydb = mysql.connector.connect(
+    # host="bwd3bxnuruinssz8cgmv-mysql.services.clever-cloud.com",
+    # user="ulijg1pspqatdfta",
+    # password="fDLM9NgbEYZTiMOXCZLP",
+    # database="bwd3bxnuruinssz8cgmv",
+    # )
     mydb = mysql.connector.connect(
-    host="bwd3bxnuruinssz8cgmv-mysql.services.clever-cloud.com",
-    user="ulijg1pspqatdfta",
-    password="fDLM9NgbEYZTiMOXCZLP",
-    database="bwd3bxnuruinssz8cgmv",
+    host="bqrdisnpyqnx6t4ifv0r-mysql.services.clever-cloud.com",
+    user="uendncgdiftgorzs",
+    password="73exox9HzKPm5A9NjYRK",
+    database="bqrdisnpyqnx6t4ifv0r",
     )
 
     #Debuggar a conexão
     print("Database: " + str(mydb))
     
     return mydb
+
--- components/funcionarios.py
+++ components/funcionarios.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 
 def mostrar_funcionarios(mydb):
+    
     from components.historico import trocar_meses_para_portugues
 
     query_setores = "SELECT DISTINCT setor_func FROM tb_funcionarios" # Consulta para obter os setores
@@ -34,33 +35,35 @@ def mostrar_funcionarios(mydb):
     resultado_matricula = cursor.fetchone()
     cursor.close()
 
+    try: 
+        query_consumos = "SELECT * FROM tb_consumo_itens WHERE matricula_func = %s"# o %s sao parametros para a consulta
+        cursor = mydb.cursor(dictionary=True)
+        cursor.execute(query_consumos, (resultado_matricula['matricula_func'],)) #Params: consulta, e os dois parametros pedidos pela consulta
+        resultado_consumos = cursor.fetchall()
+        cursor.close()
+        
+        tb_consumo_func = pd.DataFrame(resultado_consumos)
+        # Formatar a coluna 'data_cons' para o formato desejado
+        tb_consumo_func['data_cons'] = pd.to_datetime(tb_consumo_func['data_cons']).dt.strftime('%d/%b/%Y - %H:%M')
 
-    query_consumos = "SELECT * FROM tb_consumo_itens WHERE matricula_func = %s"# o %s sao parametros para a consulta
-    cursor = mydb.cursor(dictionary=True)
-    cursor.execute(query_consumos, (resultado_matricula['matricula_func'],)) #Params: consulta, e os dois parametros pedidos pela consulta
-    resultado_consumos = cursor.fetchall()
-    cursor.close()
+        tb_consumo_func['data_cons'] = tb_consumo_func['data_cons'].apply(trocar_meses_para_portugues)
 
-    tb_consumo_func = pd.DataFrame(resultado_consumos)
-    # Formatar a coluna 'data_cons' para o formato desejado
-    tb_consumo_func['data_cons'] = pd.to_datetime(tb_consumo_func['data_cons']).dt.strftime('%d/%b/%Y - %H:%M')
+        tb_consumo_func = tb_consumo_func.rename(columns={
+                "id_cons": "Número",
+                "data_cons": "Data de consumo",
+                "matricula_func": "Matrícula",
+                "nome_func": "Nome do funcionário",
+                "setor_func": "Setor",
+                "nome_prod": "Nome do produto",
+                "qtde_consumo_prod": "Quantidade",
+                "un_med_prod": "Unidade de medida",
+            })
 
-    tb_consumo_func['data_cons'] = tb_consumo_func['data_cons'].apply(trocar_meses_para_portugues)
+        tb_consumo_func = tb_consumo_func.sort_values(by="Número", ascending=False)
 
-    tb_consumo_func = tb_consumo_func.rename(columns={
-            "id_cons": "Número",
-            "data_cons": "Data de consumo",
-            "matricula_func": "Matrícula",
-            "nome_func": "Nome do funcionário",
-            "setor_func": "Setor",
-            "nome_prod": "Nome do produto",
-            "qtde_consumo_prod": "Quantidade",
-            "un_med_prod": "Unidade de medida",
-        })
-
-    tb_consumo_func = tb_consumo_func.sort_values(by="Número", ascending=False)
-
-    soma_quantidade_consumo = tb_consumo_func['Quantidade'].sum()
+        soma_quantidade_consumo = tb_consumo_func['Quantidade'].sum()
+    except Exception as e:
+        print("Erro ao carregar consumo de dados do funcionários")
 
     col1, col2 = st.columns([2.3, 1])
 
@@ -70,16 +73,20 @@ def mostrar_funcionarios(mydb):
 
 
         st.write(f"###### Matrícula: {resultado_matricula['matricula_func']}") # O colchetes serve para pegar apenas o valor, senao iria imprimir o key e o value juntos
-
-        st.write(f"###### Total de itens consumidos: {soma_quantidade_consumo}")
+        try:
+            st.write(f"###### Total de itens consumidos: {soma_quantidade_consumo}")
+        except Exception:
+            st.write("###### Total de itens consumidos: 0")
 
         st.divider()
-        st.dataframe(tb_consumo_func, use_container_width=True, hide_index=True, 
+        try:
+            st.dataframe(tb_consumo_func, use_container_width=True, hide_index=True, 
                     column_config={
             "Nome do produto": st.column_config.TextColumn(
-                "Nome do produto", width=200
-            )}
-                    )
+                "Nome do produto", width=200)})
+        except Exception:
+            st.write("Vazio")
+    
 
 
     query_pie = "SELECT nome_prod, SUM(qtde_consumo_prod) as total_consumido FROM tb_consumo_itens where matricula_func = %s GROUP BY nome_prod"
@@ -98,16 +105,20 @@ def mostrar_funcionarios(mydb):
 
 
         # Criar o gráfico de pizza usando Plotly
-    fig_pie = px.pie(df_data_pie, 
+    try:
+        fig_pie = px.pie(df_data_pie, 
                     values='total_consumido', 
                     names='nome_prod', 
                     hole=0.3, 
                     color_discrete_sequence=['#FF8C00', '#FF6F00', '#FF4500', '#C72C41'])
-        
-    fig_pie.update_layout(
+        fig_pie.update_layout(
                 width=600,   # Ajuste a largura do gráfico
-                height=420,  # Ajuste a altura do gráfico
-            )
+                height=420,)  # Ajuste a altura do gráfico
+    except Exception:
+        print("Erro ao construir grafico")
+        
+    
+            
     with col2:
         # Exibir o gráfico de pizza no Streamlit
         st.markdown(
@@ -115,4 +126,7 @@ def mostrar_funcionarios(mydb):
         <h5 style="text-align: center;">GRÁFICO DE ITENS MAIS CONSUMIDOS</h5>
         """, unsafe_allow_html=True
     )
-        st.plotly_chart(fig_pie, use_container_width=False, )
+        try:
+            st.plotly_chart(fig_pie, use_container_width=False, )
+        except Exception:
+            st.write("Nenhum produto foi consumido ainda")
